59fe42f184093aaf7c426112ac854e61f94ba832
Naga Ganesh B J <bjnaga@gmail.com>
2023-12-13 16:09:24 +0530

    Create NamedEntityRecognition.py

---
+++ nltk/NamedEntityRecognition.py
@@ -0,0 +1,41 @@
+import nltk
+
+# https://www.nltk.org/api/nltk.tokenize.punkt.html
+nltk.download('punkt') # Punkt Sentence Tokenizer
+
+# https://www.nltk.org/api/nltk.tag.perceptron.html
+nltk.download('averaged_perceptron_tagger') #  uses the perceptron algorithm to predict which POS-tag is most likely given the word.
+
+from nltk.chunk import RegexpParser
+from nltk.tokenize import word_tokenize
+
+# Example sentence
+sentence = "Educative Answers is a free web encyclopedia written by devs for devs."
+
+# # Tokenization
+tokens = word_tokenize(sentence)
+
+# # POS tagging
+pos_tags = nltk.pos_tag(tokens)
+
+# Chunking patterns
+chunk_patterns = r"""
+    NP: {<DT>?<JJ>*<NN>}  # Chunk noun phrases
+    VP: {<VB.*><NP|PP>}  # Chunk verb phrases
+"""
+
+
+# chunking is a powerful tool for analyzing sentences and extracting meaningful noun and verb phrases by 
+# grouping words together based on their grammar
+
+# Create a chunk parser
+chunk_parser = RegexpParser(chunk_patterns)
+
+# Perform chunking
+result = chunk_parser.parse(pos_tags)
+
+# Print the chunked result
+print(result)
+
+namedEnt = nltk.ne_chunk(pos_tags, binary=False)
+namedEnt.draw()
