8d34c7476bcd48180333b430bd32777750e9e966
Naga Ganesh B J <bjnaga@gmail.com>
2023-12-13 12:18:24 +0530

    Update ImageingRotation.py

--- opencv/ImageingRotation.py
+++ opencv/ImageingRotation.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 h, w = image.shape[:2]
 center = (w / 2, h / 2)
 
-# mat = cv2.getRotationMatrix2D((h,w), 90, 1)
+
 mat = cv2.getRotationMatrix2D(center, 90, 1)
 
 rotating = cv2.warpAffine(image, mat, (h, w))
